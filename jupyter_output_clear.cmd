dbeee4ad061e45dc9a954c3c2490f4efde3d3e93
march3 <gang.wei@qq.com>
2022-08-21 11:12:09 +0800

    Python超人-办公自动化

--- jupyter_output_clear.cmd
+++ jupyter_output_clear.cmd
@@ -1,17 +1,16 @@
+@echo off
+
 SET Anaconda3=C:\ProgramData\Anaconda3
 
-@echo off
 if not exist "%Anaconda3%\python.exe" (
-  echo Anaconda3 环境路径：%Anaconda3% 不存在，请手动修改 SET Anaconda3=[Anaconda3的安装路径]
+  echo %Anaconda3% envs not found!
   pause
-  goto end
 ) else (
-  echo Anaconda3 环境路径：%Anaconda3%
+  echo jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace "*.ipynb"
+  %windir%\System32\cmd.exe "/K" %Anaconda3%\Scripts\activate.bat %Anaconda3%
 )
 
-SET LANG=zh_CN.UTF8
-"%Anaconda3%\python.exe" "%Anaconda3%\cwp.py" "%Anaconda3%" "%Anaconda3%\python.exe" "%Anaconda3%\Scripts\jupyter.exe" nbconvert --ClearOutputPreprocessor.enabled=True --inplace %1
-pause
-:end
+
+
 
 
