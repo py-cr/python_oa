b809a0ef309926a8c856a571c78c5b284943baf8
march3 <gang.wei@qq.com>
2022-09-06 19:06:23 +0800

    Python超人-办公自动化

--- jupyter_output_clear.cmd
+++ jupyter_output_clear.cmd
@@ -1,16 +1,39 @@
 @echo off
-
 SET Anaconda3=C:\ProgramData\Anaconda3
 
 if not exist "%Anaconda3%\python.exe" (
   echo %Anaconda3% envs not found!
-  pause
-) else (
-  echo jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace "*.ipynb"
-  %windir%\System32\cmd.exe "/K" %Anaconda3%\Scripts\activate.bat %Anaconda3%
+  goto end
+) 
+
+if "%1" == "" (
+  goto input
 )
 
+goto exec
+:input
+SET ipynb=
+cls
+echo 【清空 jupyter notebook 所有的输出】
+echo 请输入 .ipynb 文件的路径 或者 拖动文件到这，按回车（为空回车则退出） 
+SET /p ipynb=
 
-
+if "%ipynb%" == "" (
+  goto end
+) else (
+    CALL %Anaconda3%\Scripts\activate.bat %Anaconda3%
+    CALL conda activate base
+    jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace "%ipynb%"
+	pause
+    goto input
+  )
+  
+:exec
+cls
+CALL %Anaconda3%\Scripts\activate.bat %Anaconda3%
+CALL conda activate base
+jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace "%1"
+pause
+:end
 
 
